ad1e8ed8644dac6fdffc7eeac93a97ef24b93ac9
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-12-29 22:50:33 +0800

    done with multiple colors

--- content/topic_5_list/multiple_color_example.py
+++ content/topic_5_list/multiple_color_example.py
@@ -9,13 +9,15 @@ print(colors)
 # Drawing a square like we did in loops
 pen = turtle.Turtle() # Making the pen we are using to draw
 for i in range(4): # For each side to draw, i goes from 0 - 3 (position 1 -4)
-    pen.color(colors[i])
+    pen.color(colors[i]) # Add the color
     pen.forward(100) # Draw a side of 100
     pen.right(90) # Make a right turn
 
 # Another way to do it
 pen = turtle.Turtle() # Making the pen we are using to draw
 for color in colors: # For each color in colors
-    
+    pen.color(color) # Add the color
+    pen.forward(100) # Draw a side of 100
+    pen.right(90) # Make a right turn
 
 turtle.exitonclick() # For VSCODE so it does not immdeiately close
